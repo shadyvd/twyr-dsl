fe02fb30f1e37923b840ad98565d5407bffad16d
Vish Desai <shadyvd@hotmail.com>
2020-01-30 18:00:31 +0530

    Minor changes
    Signed-off-by: Vish Desai <shadyvd@hotmail.com>

--- addon/components/twyr-radio-group/index.js
+++ addon/components/twyr-radio-group/index.js
@@ -32,6 +32,26 @@ export default class TwyrRadioGroupComponent extends Component {
 	}
 	// #endregion
 
+	// #region Lifecycle Hooks
+	@action
+	didInsert(element) {
+		this.debug(`didInsert`);
+		this._element = element;
+
+		if(this.args.registerWithParent && (typeof this.args.registerWithParent === 'function'))
+			this.args.registerWithParent(this, true);
+	}
+
+	willDestroy() {
+		this.debug(`willDestroy`);
+
+		if(this.args.registerWithParent && (typeof this.args.registerWithParent === 'function'))
+			this.args.registerWithParent(this, false);
+
+		super.willDestroy(...arguments);
+	}
+	// #endregion
+
 	// #region DOM Event Handlers
 	@action
 	handleKeydown(event) {
