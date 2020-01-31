8ec87caf69b502481fd6e4418cb790425f0c5bf7
Vish Desai <shadyvd@hotmail.com>
2020-01-31 08:32:18 +0530

    package.json changes
    Signed-off-by: Vish Desai <shadyvd@hotmail.com>

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-	'extends': 'octane'
+	'extends': 'octane',
+	'rules': {
+		'no-inline-styles': false,
+		'no-invalid-interactive': false,
+		'no-positive-tabindex': false
+	}
 };
